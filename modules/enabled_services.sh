5bd6e65948b603d2a8d39d47d2ef0cbe7563d7ac
Lee Keitel <lee@keitel.xyz>
2017-08-07 09:49:25 -0500

    Don't break parse on error

--- modules/enabled_services.sh
+++ modules/enabled_services.sh
@@ -3,13 +3,13 @@
 
 sysctl="$(which systemctl 2>/dev/null)"
 if [ -z "$sysctl" ]; then
-    echo "systemctl not available"
+    echo "[{\"name\": \"systemctl not available\"}]"
     exit 1
 fi
 
-systemctl list-unit-files --no-page |\
-    grep '.service' |\
-    grep 'enabled' |\
-    cut -d' ' -f1 |\
-    /usr/bin/awk -F: 'BEGIN {print "["} {print "{\"name\": \"" $1 "\"}," } END {print "]"}' |\
-    /bin/sed 'N;$s/,\n/\n/;P;D'
+systemctl list-unit-files --no-page \
+    | grep '.service' \
+    | grep 'enabled' \
+    | cut -d' ' -f1 \
+    | /usr/bin/awk -F: 'BEGIN {print "["} {print "{\"name\": \"" $1 "\"}," } END {print "]"}' \
+    | /bin/sed 'N;$s/,\n/\n/;P;D'
